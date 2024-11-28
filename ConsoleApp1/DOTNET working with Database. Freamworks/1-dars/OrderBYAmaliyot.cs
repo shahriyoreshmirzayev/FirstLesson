90f932c5676df5f4482bdccdb24873bdba851cb0
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-11-28 16:39:06 +0500

    Student classi ichidan Brachlar ichidan OrderBy qilish Method va Query Syntax lar orqali

--- ConsoleApp1/DOTNET working with Database. Freamworks/1-dars/OrderBYAmaliyot.cs
+++ ConsoleApp1/DOTNET working with Database. Freamworks/1-dars/OrderBYAmaliyot.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._1_dars
+﻿namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._1_dars
 {
     class OrderBYAmaliyot
     {
         public static void OrderBY()
         {
-            OrderBy_Amaliyot_1();
+            //OrderBy_Amaliyot_1();
+            OrderByStudents();
         }
         public static void OrderBy_Amaliyot_1()
         {
@@ -32,6 +27,43 @@ namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._1_dars
             {
                 Console.Write($"{number} ");
             }
+        }
+        public static void OrderByStudents()
+        {
+            //Method Syntax
+            var MethodSyntax = Student.GetAllStudents().OrderBy(x => x.Branch).ToList();
+            Console.WriteLine("Method Syntax");
+            foreach (var number in MethodSyntax)
+            {
+                Console.WriteLine("Branch: " + number.Branch + ", Name: " + number.Name + ", Age: " + number.Age + ", Male: " + number.Gender + ", ID: " + number.ID);
+            }
+            //Query Syntax
+            /*Console.WriteLine("Query Syntax");
+            var QuerySyntax = (from std in Student.GetAllStudents()
+                               orderby std.Branch
+                               select std);
+            foreach (var number in QuerySyntax)
+            {
+                Console.WriteLine("Branch: " + number.Branch + ", Name: " + number.Name + ", Gender: " + number.Gender + ", Age: " + number.Age + ", ID: " + number.ID);
+            }*/
+        }
+    }
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Branch { get; set; }
+        public int Age { get; set; }
+        public static List<Student> GetAllStudents()
+        {
+            return new List<Student>()
+            {
+                new Student { ID = 1001, Name = "Azamat", Gender = "Male", Branch = "Shahar", Age = 19 },
+                new Student { ID = 1002, Name = "Dilshod", Gender = "Male", Branch = "Tuman", Age = 24 },
+                new Student { ID = 1003, Name = "Jo'rabek", Gender = "Male", Branch = "Viloyat", Age = 20 },
+                new Student { ID = 1004, Name = "Lola", Gender = "Female", Branch = "Qishloq", Age = 35 }
+            };
         }
     }
 }
