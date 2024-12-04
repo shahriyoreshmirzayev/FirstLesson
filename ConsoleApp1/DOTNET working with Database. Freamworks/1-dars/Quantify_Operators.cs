2ffc6747016a6ea18f3c0707f745d2443c0e8a61
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-12-04 19:28:41 +0500

    ANY amaliyot

--- ConsoleApp1/DOTNET working with Database. Freamworks/1-dars/Quantify_Operators.cs
+++ ConsoleApp1/DOTNET working with Database. Freamworks/1-dars/Quantify_Operators.cs
@@ -5,7 +5,10 @@
         public static void Start()
         {
             //All1();
-            All2();
+            //All2();
+
+            //Any1();
+            Any2();
         }
         public static void All1()
         {
@@ -29,13 +32,44 @@
             //Using Method Syntax
             bool methodSyntax = stringArray.All(name => name.Length > 5);
             Console.WriteLine("Method Syntax");
-            Console.WriteLine("Batcha ismlar uzunligi 5 dan katta-mi? " + methodSyntax);
+            Console.WriteLine("Barcha ismlar uzunligi 5 dan katta-mi? " + methodSyntax);
 
             //Using Query Syntax
             bool querySyntax = (from name in stringArray
                                 select name).All(name => name.Length > 3);
             Console.WriteLine("Query Syntax");
             Console.WriteLine($"Barcha ismlar uzunligi 5 dan katta-mi? {querySyntax}");
+        }
+        public static void Any1()
+        {
+            int[] intArray = { 11, 15, 59, 35, 13, 56, 100 };
+
+            //Using Method Syntax
+            bool methodSyntax = intArray.Any(x => x < 10);
+            Console.WriteLine("Method Syntax");
+            Console.WriteLine("Massivda biror bir element 10 dan kichikmi? " + methodSyntax);
+
+            //Using Query Syntax
+            bool querySyntax = (from name in intArray
+                                select name).Any(x => x < 10);
+            Console.WriteLine("Query Syntax");
+            Console.WriteLine("Masssivda biror bir element 10 dan kichikmi? " + querySyntax );
+        }
+        public static void Any2()
+        {
+            string[] stringArray = { "James", "Lochin", "Ruslan", "Allegri", "Sara" };
+
+            //Using Method Syntax
+            bool methodSyntax = stringArray.Any(name => name.Length > 5);
+            Console.WriteLine("Method Syntax");
+            Console.WriteLine($"Massiv ichida uzunligi 5 dan katta bo'lgan ismlar bormi? {methodSyntax}");
+
+            //Using Query Syntax
+            bool querySyntax = (from name in stringArray
+                                select name).Any(name => name.Length > 3);
+            Console.WriteLine("Query Syntax");
+            Console.WriteLine("Massiv ichida uzunligi 5 dan katta bo'lgan ismlar bormi? " + querySyntax);
+            Console.ReadKey();
         }
     }
 }
