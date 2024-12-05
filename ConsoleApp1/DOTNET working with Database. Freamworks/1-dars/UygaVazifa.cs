8c98da28a91a6c1d1dfa1c1cdcdcb3392858c99d
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-12-05 15:02:48 +0500

    Uyga vazifa

---
+++ ConsoleApp1/DOTNET working with Database. Freamworks/1-dars/UygaVazifa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._1_dars
+{
+    internal class UygaVazifa
+    {
+    }
+}
