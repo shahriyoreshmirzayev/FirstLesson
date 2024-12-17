13bcb208616fe2145ed09223d2103c51ac4710a3
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-12-17 16:38:29 +0500

    Take

--- ConsoleApp1/DOTNET working with Database. Freamworks/3-dars/Partititioning_Operators.cs
+++ ConsoleApp1/DOTNET working with Database. Freamworks/3-dars/Partititioning_Operators.cs
@@ -8,7 +8,11 @@ namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._3_dars
             //Take1();
             //Take2();
             //Take3();
-            Take4();
+            //Take4();
+
+            //TakeWithException();
+
+            TakeWhile1();
         }
         public static void Take1()
         {
@@ -84,7 +88,7 @@ namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._3_dars
             {
                 Console.Write($"{i} ");
             }
-            Console.Read();
+            Console.ReadKey();
         }
         public static void Take4()
         {
@@ -92,21 +96,81 @@ namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._3_dars
             List<Employee> employees = Employee.GetEmployees();
             //Using Method Syntax
             var ResultMS = employees.OrderByDescending(emp => emp.Salary).Take(3).ToList();
-            foreach(var emp in ResultMS)
+            Console.WriteLine("Method Syntax");
+            foreach (var emp in ResultMS)
             {
                 Console.WriteLine($"Name: {emp.Name}, Gender: {emp.Gender}, Salary: {emp.Salary}");
             }
+
+            //Using Query Syntax
+            var ResultQS = (from emp in Employee.GetEmployees()
+                            orderby emp.Salary descending
+                            select emp)
+                            .Take(3).ToList();
+            Console.WriteLine("Query Syntax");
+            foreach (var item in ResultQS)
+            {
+                Console.WriteLine($"Name: {item.Name}, ID: {item.ID}, Gender: {item.Gender}");
+            }
             Console.Read();
         }
-        public class Employee
+        public static void TakeWithException()
         {
-            public int ID { get; set; }
-            public string Name { get; set; }
-            public string Gender { get; set; }
-            public int Salary { get; set; }
-            public static List<Employee> GetEmployees()
+            List<int> numbers = null;
+
+            //Using Method Syntax
+            var ResultMS = numbers.Take(5).ToList();
+            Console.WriteLine("Method Syntax");
+            foreach (var x in ResultMS)
             {
-                return new List<Employee>
+                Console.WriteLine(x);
+            }
+            //Using Query Syntax
+            var ReslutQS = (from x in numbers
+                            select x)
+                           .Take(5).ToList();
+            Console.WriteLine("Query Syntax");
+            foreach (var x in ReslutQS)
+            {
+                Console.WriteLine(x);
+            }
+            Console.Read();
+        }
+        public static void TakeWhile1()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+
+            //Using Method Syntax
+            List<int> ResultMS = numbers.TakeWhile(num => num < 6).ToList();
+            Console.WriteLine("Method Syntax");
+            foreach (var num in ResultMS)
+            {
+                Console.Write($"{num} ");
+            }
+            Console.WriteLine();
+
+            //Using Query Syntax
+            List<int> ReslutQS = (from num in numbers
+                                  select num)
+                                 .TakeWhile(num => num < 6)
+                                 .ToList();
+            Console.WriteLine("Query Syntax");
+            foreach (var num in ReslutQS)
+            {
+                Console.Write(num + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+        public static List<Employee> GetEmployees()
+        {
+            return new List<Employee>
                 {
                 new Employee { ID = 1, Name = "Asadbek", Gender = "Male", Salary = 10000 },
                 new Employee { ID = 2, Name = "Jasurbek", Gender = "Male", Salary = 40000 },
@@ -117,11 +181,10 @@ namespace ConsoleApp1.DOTNET_working_with_Database._Freamworks._3_dars
                 new Employee { ID = 7, Name = "Ilhom", Gender = "Male", Salary = 30000 },
                 new Employee { ID = 8, Name = "Behzod", Gender = "Male", Salary = 15000 }
                 };
-            }
-            //internal static List<Employee> GetAllEmployees()
-            //{
-            //    throw new NotImplementedException();
-            //}
         }
+        //internal static List<Employee> GetAllEmployees()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
