74c903369552e96fc999bd430e444e9e4782840f
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-12-19 19:52:57 +0500

    LastOrDefaults metodlarini amaliyoti

--- ConsoleApp1/DOTNET working with Database. Freamworks/3-dars/Element_Operators.cs
+++ ConsoleApp1/DOTNET working with Database. Freamworks/3-dars/Element_Operators.cs
@@ -5,7 +5,9 @@
         public static void Start()
         {
             //ElementAt();
-            ElementAt2();
+            //ElementAt2();
+            //First();
+            Last();
         }
         public static void ElementAt()
         {
@@ -20,10 +22,10 @@
                             ElementAt(4);
             Console.WriteLine("Method Syntax: " + ResultMS);
             Console.WriteLine("Query Syntax: " + ResultQS);
-            
         }
         public static void ElementAt2()
         {
+            //Default qiymay qaytarmydi 
             List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             //Method Syntax
             var ResultMS = numbers.ElementAtOrDefault(56);
@@ -33,6 +35,32 @@
                             .ElementAtOrDefault
                             (56);
             Console.WriteLine(ResultQS);
+            Console.Read();
+        }
+        public static void First()
+        {
+            //bu method birinchi uchragan elementni qaytaradi
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Using Query Syntax
+            var ResultQS = (from num in numbers
+                            select num).First(num => num == 5);
+            Console.WriteLine(ResultQS);
+
+            //Using Method Syntax
+            var ResultMS = numbers.FirstOrDefault(number => number == 5);
+            Console.WriteLine(ResultMS);
+        }
+        public static void Last()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 5 };
+            //Using Query Syntax
+            var ResultQS = (from num in numbers
+                           select num).Last(num => num == 5);
+
+            //Using Method Syntax
+            var ResultMS = numbers.LastOrDefault(number => number == 65);
+            Console.WriteLine("Method Syntax: " + ResultMS);
+            Console.WriteLine("Query Syntax: " + ResultQS);
         }
     }
 }
