cb401f4536a3d81532036fb45bbdcb9f297bcb10
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2024-12-20 12:36:55 +0500

    Single Operators

--- ConsoleApp1/DOTNET working with Database. Freamworks/3-dars/Element_Operators.cs
+++ ConsoleApp1/DOTNET working with Database. Freamworks/3-dars/Element_Operators.cs
@@ -7,7 +7,9 @@
             //ElementAt();
             //ElementAt2();
             //First();
-            Last();
+            //Last();
+
+            Single();
         }
         public static void ElementAt()
         {
@@ -55,12 +57,35 @@
             List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 5 };
             //Using Query Syntax
             var ResultQS = (from num in numbers
-                           select num).Last(num => num == 5);
+                            select num).Last(num => num == 5);
 
             //Using Method Syntax
             var ResultMS = numbers.LastOrDefault(number => number == 65);
-            Console.WriteLine("Method Syntax: " + ResultMS);
             Console.WriteLine("Query Syntax: " + ResultQS);
+            Console.WriteLine("Method Syntax: " + ResultMS);
+        }
+        public static void Single()
+        {
+            Employee? MethodSyntax = Employee.GetEmployees().SingleOrDefault(emp => emp.Name == "Asad1");
+            Console.WriteLine(MethodSyntax);
+            Console.ReadKey();
+        }
+    }
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public static List<Students> GetStudents()
+        {
+            return new List<Students>()
+            {
+                new Students  { Id = 1, Name = "Asad1", Department = "IT" },
+                new Students  { Id = 2, Name = "Asad2", Department = "HR" },
+                new Students  { Id = 3, Name = "Asad3", Department = "IT" },
+                new Students  { Id = 4, Name = "Asad4", Department = "IT" },
+                new Students  { Id = 5, Name = "Asad5", Department = "HR" }
+            };
         }
     }
 }
