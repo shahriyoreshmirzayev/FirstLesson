c4d1df907a5b87b3c43b9aac34e3af8676f810a0
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-07-15 11:55:07 +0500

    EKUB, EKUK, Tub sonlikka tekshirish, Rqamlar yig'indisi, Sonni teskarisini chiqarish

--- ConsoleApp1/GITASavollar.cs
+++ ConsoleApp1/GITASavollar.cs
@@ -5,7 +5,11 @@ public class GITASavollar
     public void Start()
     {
         //Masala1();
-        Masala2();
+        //Masala2();
+        //Masala3();
+        //Masala4();
+        //Masala5();
+        Masala6();
     }
     public void Masala1()
     {
@@ -29,5 +33,83 @@ public class GITASavollar
             s += str[i];
         }
         Console.WriteLine($"Sonning teskari ko'rinishi: {s}");
+    }
+    public void Masala3()
+    {
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        for(int i = 1; i <= number; i++)
+        {
+            if (number % i == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 2)
+        {
+            Console.WriteLine($"{number} soni tub sondir.");
+        }
+        else
+        {
+            Console.WriteLine($"{number} soni tub son emas.");
+        }
+    }
+    public void Masala4()
+    {
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        for(int i = 1; i <= number; i++)
+        {
+            count = 0;
+            for(int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    count++;
+                }
+            }
+            if(count == 2)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+        Console.Read();
+    }
+    public void Masala5()
+    {
+        Console.Write("Enter number: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int ekub = 1;
+        for(int i = 1; i <= number1; i++)
+        {
+            if(number1 % i == 0 && number2 % i == 0)
+            {
+                ekub = i;
+            }
+        }
+        Console.WriteLine($"Ekub: {ekub}");
+        Console.Read();
+    }
+    public void Masala6()
+    {
+        Console.Write("Enter number1: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter number2: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int ekuk = 1;
+        for(int i = 1; i <= number1 * number2; i++)
+        {
+            if(i % number1 == 0 && i % number2 == 0)
+            {
+                ekuk = i;
+                break;
+            }
+        }
+        Console.WriteLine($"Ekuk: {ekuk}");
+        Console.Read();
     }
 }
